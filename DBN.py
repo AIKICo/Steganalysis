53108a6a944608ace62289b9bc410e9be6ae9bb6
qermezkon@gmail.com <qermezkon@gmail.com>
2017-07-12 16:12:33 +0430

    Full Code

--- DBN.py
+++ DBN.py
@@ -5,6 +5,7 @@ from sklearn.metrics.classification import accuracy_score
 from sklearn.metrics import classification_report
 from dbn.tensorflow import SupervisedDBNClassification
 #from dbn import SupervisedDBNClassification
+from sklearn.model_selection import cross_val_score
 
 
 def loadData(filename,instanceCol):
@@ -16,21 +17,27 @@ def loadData(filename,instanceCol):
         y.append(row[-1]);
     return np.array(x[1:]).astype((np.float32)), np.array(y[1:]).astype(np.int);
 
+scores = [];
 X,Y = loadData('/home/mohammad/Documents/python/Steganalysis/feature.csv',99);
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0);
 
-classifier = SupervisedDBNClassification(hidden_layers_structure=[1024,1024,1024,1024],
-                                         learning_rate_rbm=0.05,
-                                         learning_rate=0.1,
-                                         n_epochs_rbm=20,
-                                         n_iter_backprop=2000,
-                                         batch_size=32,
-                                         activation_function='relu', #relu,sigmoid
-                                         dropout_p=0.2);
-classifier.fit(X_train, Y_train);
-Y_pred = classifier.predict(X_test);
+for i in range(10):
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0);
+    classifier = SupervisedDBNClassification(hidden_layers_structure=[256,128,256],
+                                             learning_rate_rbm=0.05,
+                                             learning_rate=0.1,
+                                             n_epochs_rbm=10,
+                                             n_iter_backprop=1000,
+                                             batch_size=32,
+                                             activation_function='relu', #relu,sigmoid
+                                             dropout_p=0.2);
 
-print(Y_test);
-print(Y_pred);
-print('Done.\nAccuracy: %f' % accuracy_score(Y_test, Y_pred))
-print(classification_report(Y_test, Y_pred));
+    classifier.fit(X_train, Y_train);
+    Y_pred = classifier.predict(X_test);
+    scores.append(accuracy_score(Y_test, Y_pred));
+#print(Y_test);
+#print(Y_pred);
+#print('Done.\nAccuracy: %f' % accuracy_score(Y_test, Y_pred))
+#print(classification_report(Y_test, Y_pred));
+
+print(scores);
+print(np.mean(scores));
