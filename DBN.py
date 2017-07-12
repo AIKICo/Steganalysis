69a77febe14e60242c23191b59ede9f9511ced4b
qermezkon@gmail.com <qermezkon@gmail.com>
2017-07-12 12:35:48 +0430

    Full Code

--- DBN.py
+++ DBN.py
@@ -19,19 +19,18 @@ def loadData(filename,instanceCol):
 X,Y = loadData('/home/mohammad/Documents/python/Steganalysis/feature.csv',99);
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0);
 
-classifier = SupervisedDBNClassification(hidden_layers_structure=[256,256],
+classifier = SupervisedDBNClassification(hidden_layers_structure=[1024,1024,1024,1024],
                                          learning_rate_rbm=0.05,
                                          learning_rate=0.1,
-                                         n_epochs_rbm=10,
-                                         n_iter_backprop=10000,
+                                         n_epochs_rbm=20,
+                                         n_iter_backprop=2000,
                                          batch_size=32,
                                          activation_function='relu', #relu,sigmoid
                                          dropout_p=0.2);
 classifier.fit(X_train, Y_train);
 Y_pred = classifier.predict(X_test);
 
-print('Done.\nY Test: ' % Y_test);
-print('Done.\nY Pred: ' % Y_pred);
-
+print(Y_test);
+print(Y_pred);
 print('Done.\nAccuracy: %f' % accuracy_score(Y_test, Y_pred))
-print('Done.\nAccuracy: ' % classification_report(Y_test, Y_pred));
+print(classification_report(Y_test, Y_pred));
