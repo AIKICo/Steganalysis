c170951e00efdd9f214204da47a3ada8b465ddee
qermezkon@gmail.com <qermezkon@gmail.com>
2017-07-12 10:29:59 +0430

    Full Code

--- DBN.py
+++ DBN.py
@@ -1,22 +1,33 @@
 import csv
 import numpy as np
-import matplotlib.pyplot as plt
-import sys
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error, explained_variance_score
-from sklearn.utils import shuffle
-from sklearn.model_selection import train_test_split
-from pandas import Series, DataFrame,read_csv
 from sklearn.model_selection import train_test_split
 from sklearn.metrics.classification import accuracy_score
-#from dbn.tensorflow import SupervisedDBNClassification
-from dbn import SupervisedDBNClassification
+from dbn.tensorflow import SupervisedDBNClassification
+#from dbn import SupervisedDBNClassification
 
-csv = read_csv('/home/mohammad/Documents/python/Steganalysis/feature.csv')
-train, test=train_test_split(csv, train_size=0.8);
 
-x_train=np.asarray(np.asmatrix(train)[:,0:99]);
-y_train=np.asarray(np.asmatrix(train)[:,100]);
+def loadData(filename,instanceCol):
+    file_reader = csv.reader(open(filename,'r'),delimiter=',')
+    x = [];
+    y = [];
+    for row in file_reader:
+        x.append(row[0:instanceCol]);
+        y.append(row[-1]);
+    return np.array(x[1:]).astype((np.float32)), np.array(y[1:]).astype(np.int);
 
-x_test=np.asarray(np.asmatrix(test)[:,0:99]);
-y_test=np.asarray(np.asmatrix(test)[:,100]);
+
+
+X,Y = loadData('/home/mohammad/Documents/python/Steganalysis/feature.csv',99);
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0);
+
+classifier = SupervisedDBNClassification(hidden_layers_structure=[1024,1024],
+                                         learning_rate_rbm=0.05,
+                                         learning_rate=0.1,
+                                         n_epochs_rbm=10,
+                                         n_iter_backprop=10,
+                                         batch_size=32,
+                                         activation_function='relu',
+                                         dropout_p=0.2)
+classifier.fit(X_train, Y_train)
+Y_pred = classifier.predict(X_test)
+print('Done.\nAccuracy: %f' % accuracy_score(Y_test, Y_pred))
