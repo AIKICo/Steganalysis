536ec61d6161d9c98242311d38c9457064395893
qermezkon@gmail.com <qermezkon@gmail.com>
2017-07-16 15:55:46 +0430

    Add 90b, 500b, 1000b, 1400b dataset

--- StegFile.py
+++ StegFile.py
@@ -2,7 +2,7 @@ import os;
 from subprocess import Popen, PIPE
 
 root, dirs, files = next(os.walk('/home/mohammad/Documents/python/Steganalysis/steg'))
-secret_filename = '1000.txt'
+secret_filename = '1400.txt'
 for file in files:
     if file.lower().endswith('.wav'):
         p = Popen('steghide embed -cf /home/mohammad/Documents/python/Steganalysis/steg/' + file + ' --dontcompress -ef  /home/mohammad/Documents/python/Steganalysis/steg/' + secret_filename + ' -p kabinet95', shell=True,
